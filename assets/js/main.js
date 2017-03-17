16bdf0927682f2204b83f26b58ad35c4a373a652
Jim Skogman <yim.apichai@gmail.com>
2017-03-17 10:01:15 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    "use strict";
+    $('body').addClass('preload-site');
+
+    // Scroll To Top
+    $('body').prepend('<div class="go-top"><span id="top"><i class="ion-chevron-up"></i></span></div>');
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 500){
+            $('.go-top').fadeIn(600);
+        }else{
+            $('.go-top').fadeOut(600);
+        }
+    });
+    $('#top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800, 'easeInQuad');
+        return false;
+    });
+
+    // Match Height
+    $('.items-row').each(function() {
+        $(this).children('.single-item').matchHeight();
+    });
+  
+});
+
+$(window).load(function(){
+	$('#preloader').fadeOut();
+	$('.preloader-spinner').fadeOut(400);
+	$('body').removeClass('preload-site');
+});
